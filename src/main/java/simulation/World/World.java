1348c7fe110482d3cbb703890b23045987a67f07
tunyaa <ildcampthunder@gmail.com>
2026-02-25 00:11:26 +0300

    OK: +feat генерация камня на карте. каменный фрагмент 0-3 в одну линию

--- src/main/java/simulation/World/World.java
+++ src/main/java/simulation/World/World.java
@@ -35,15 +35,16 @@ public class World {
     // Заполняет карту сущностями
     public void generateEntitysOnWorldField() {
 
-        for (int i = 1; i <= worldMapComponent.getWorldLen(); i++) {
-//        for (int i = 0; i < worldMapComponent.getLen() / 10; i++) {
-            int nextInt = random.nextInt(worldMapComponent.getWorldLen());
-            Stone stone = new Stone();
-            stone.setPosition(i);
-            positionEntityMap.put(i, stone);
-//            stone.setPosition(nextInt);
-//            positionEntityMap.put(nextInt, stone);
-        }
+//        for (int i = 1; i <= worldMapComponent.getWorldLen(); i++) {
+        ////        for (int i = 0; i < worldMapComponent.getLen() / 10; i++) {
+//            int nextInt = random.nextInt(worldMapComponent.getWorldLen());
+//            Stone stone = new Stone();
+//            stone.setPosition(i);
+//            positionEntityMap.put(i, stone);
+////            stone.setPosition(nextInt);
+////            positionEntityMap.put(nextInt, stone);
+//        }
+    generateStones();
     }
 
     private void generateStones() {
@@ -87,28 +88,61 @@ public class World {
     }
 
     // Проверка. Позиция выходит за край.
-    private boolean isPositionNotOutOfBorder(int position, int newPosition, World world) {
+    private boolean isPositionNotOutOfBorder(int position, int newPosition, World world, int derection) {
         int width = world.getWorldMap().getWidth();
         int height = world.getWorldMap().getHeight();
         int len = world.getWorldMap().getWorldLen();
         int positionH = (int) Math.ceil((double) position / width);
         int positionW = width - (positionH * width - position);
 
-        if (newPosition >= 1) {
-            position = newPosition;
-            return true;
-        } else if (position < len) {
-            position = newPosition;
-            return true;
-        } else if (newPosition > (width * (position / width))) {
-            position = newPosition;
-            return true;
-        } else if (position < (width * ((position - 1) / width + 1))) {
-            position += 1;
-            return true;
+        if (derection == 0) {
+            if (newPosition >= 1) {
+                position = newPosition;
+                return true;
+            }
+        }
+        if (derection == 1) {
+            if (newPosition < len) {
+                position = newPosition;
+                return true;
+            }
+        }
+        if (derection == 2) {
+            if (newPosition > (width * (position / width))) {
+                position = newPosition;
+                return true;
+            }
+        }
+        if (derection == 3) {
+            if (position < (width * ((position - 1) / width + 1))) {
+                position += 1;// Сделать = new Position
+                return true;
+            }
         }
         return false;
 
+//            newPoint = point - width;
+//            if (newPoint >= 1) {
+//                point = newPoint;
+//            }
+//        }
+//        if (direction.equals("s")) {
+//            newPoint = point + width;
+//            if (point < len) {
+//                point = newPoint;
+//            }
+//        }
+//        if (direction.equals("a")) {
+//            newPoint = point - 1;
+//            if (newPoint > (width * (point / width))) {
+//                point = newPoint;
+//            }
+//        }
+//        if (direction.equals("d")) {
+//            if (point < (width * ((point - 1) / width + 1))) {
+//                point += 1;
+//            }
+//        }
     }
     // Формирование линии из камня
 
@@ -117,9 +151,10 @@ public class World {
         int stoneCount = 0;
         int maxSubsequence;
         int direction = random.nextInt(4);
-        int len = getWorldMap().getWorldLen();
-        int[] ary = new int[]{-len, len, -1, 1};
-
+        int height = getWorldMap().getHeight();
+                // вверх, вниз, влево, вправо
+        int[] ary = new int[]{-height, height, -1, 1};
+        System.out.println(direction + " - Направление");
         int steps = random.nextInt(4);
         for (int i = 0; i < steps; i++) {
 
@@ -127,8 +162,11 @@ public class World {
             stone.setPosition(position);
             positionEntityMap.put(position, stone);
             stoneCount++;
+            System.out.println("Position - " + position);
+            
             int newPosition = position + ary[direction];
-            if (isPositionNotOutOfBorder(position, newPosition, this)) {
+            System.out.println("NewPosition - " + newPosition);
+            if (isPositionNotOutOfBorder(position, newPosition, this, direction)) {
                 position = newPosition;
             } else {
                 break;
