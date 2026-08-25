29414878dd01b1b32b237ea37c3dde2a0862da86
Tunyaa <oldcampthunder@gmail.com>
2026-08-25 15:12:02 +0300

    delete comment

--- src/main/java/simulation/Model/Viewer/SquareViewer.java
+++ src/main/java/simulation/Model/Viewer/SquareViewer.java
@@ -14,7 +14,7 @@ import simulation.World.World;
 public class SquareViewer implements Viewer {
 
     private Creature creature;
-    private  Class<?> target;
+    private Class<?> target;
 
     public SquareViewer(Creature creature, Class<?> target) {
         this.creature = creature;
@@ -52,44 +52,27 @@ public class SquareViewer implements Viewer {
                 int topExtremePoint = rowColumn.getRow() - rangeOfView;
                 int lowerExtremePoint = rowColumn.getRow() + rangeOfView;
 
-//                LeftExtremePoint = LeftExtremePoint < 1 ? 1 : LeftExtremePoint;
-//                RightExtremePoint = RightExtremePoint > width ? width : RightExtremePoint;
-//                topExtremePoint = topExtremePoint < 1 ? 1 : topExtremePoint;
-//                lowerExtremePoint = lowerExtremePoint > world.getWorldGrid().getHeight() ? 1 : lowerExtremePoint;
                 RowColumn rowColumnByScanPosition = world.getWorldGrid().getRowColumnByPosition(f);
                 int col = rowColumnByScanPosition.getCol();
                 int row = rowColumnByScanPosition.getRow();
 
                 if (col >= LeftExtremePoint && col <= RightExtremePoint
                         && row >= topExtremePoint && row <= lowerExtremePoint) {
-//                    List<Entity> get = world.getPositionEntityMap().get(f);
                     List<Entity> e = world.getEntitysByPosition(f);
                     if (e != null) {
-                        
+
                         if (!e.isEmpty()) {
-                            
+
                             for (Entity entity : e) {
-                               
+
                                 if (target.isInstance(entity)) {
 
-                                creature.setTargetPosition(f);
+                                    creature.setTargetPosition(f);
+                                }
                             }
-                            }
-                            
+
                         }
                     }
-
-//                    if (get != null) {
-//                        for (Entity entity : get) {
-//
-//                            if (entity instanceof Grass) {
-//
-//                                creature.setTargetPosition(f);
-//                            }
-//
-//                        }
-//                    }
-                } else {
 
                 }
 
